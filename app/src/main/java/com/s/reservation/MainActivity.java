08616d23cc0f4ad7fb6c53cb3e381b68c539a431
songjuhan <wngks8874@naver.com>
2022-05-25 15:53:27 +0900

    first upload

---
+++ app/src/main/java/com/s/reservation/MainActivity.java
@@ -0,0 +1,15 @@
+package com.s.reservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation);
+
+    }
+}
